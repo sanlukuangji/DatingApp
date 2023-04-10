d2db6c2ff9e935680ca2753e9fcc179bb010237b
sanlukuangji <13611051332@163.com>
2023-04-09 17:56:35 -0700

    End of Section 3

--- Program.cs
+++ Program.cs
@@ -10,10 +10,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+// 配置跨域资源共享
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+// UseCors启用Cors中间件，配置Cors允许客户端的任何请求头，任何请求方法以及来自4200域的请求
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
